7de5b8adc7c06f488c1ab9a86afe73ca90ef9ccd
Adam Vandolder <adam.vandolder@gmail.com>
2019-10-04 11:23:04 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from typing import List
+import sys
+
+import pandas as pd
+
+
+class ElectoralProjection:
+    """Construct an electoral projection using a Monte Carlo simulation."""
+
+    def __init__(self):
+        pass
+
+
+def main(args: List[str]) -> int:
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
