f963b684ecf56de13f9e4f7eb5a151527b6e670d
Adam Vandolder <adam.vandolder@gmail.com>
2019-10-04 14:26:04 -0400

    Process 2015 results and generate simplified data

--- src/process_2015.py
+++ src/process_2015.py
@@ -1,23 +1,59 @@
 # Process the raw 2015 electoral district data into a simpler form.
+# Should be used with the data from
+# https://www.elections.ca/res/rep/off/ovr2015app/41/data_donnees/pollbypoll_bureauparbureauCanada.zip
 
 from os import walk, path
 
 import pandas as pd
 
 # Read in each district and aggregate it.
-(path, _, district_files) = next(walk('data/2015/'))
+district_headers = ['prov', 'name', 'votes', 'cpc', 'ndp', 'lpc', 'bq', 'gpc', 'other']
+districts = pd.DataFrame(columns=district_headers)
+
+(dirpath, _, district_files) = next(walk('data/2015/'))
 headers = ['id', 'name', 'party', 'votes']
+parties = {
+    'Conservative': 'cpc',
+    'NDP-New Democratic Party': 'ndp',
+    'Liberal': 'lpc',
+    'Green Party': 'gpc',
+    'Bloc Québécois': 'bq',
+}
 keep_columns = [0, 1, 13, 17]
 for f in district_files:
     df = pd.read_csv(
-        path.join(path, f),
+        path.join(dirpath, f),
         header=0,
         names=headers,
         usecols=keep_columns,
     )
-    parties = df.groupby('party', sort=False)
-    parties_by_vote = parties.agg({
+    group_by_party = df.groupby('party', sort=False)
+    parties_by_vote = group_by_party.agg({
         'id': 'first',
         'name': 'first',
         'votes': 'sum',
     })
+
+    district = {
+        'id': [parties_by_vote['id'].iloc[0]],
+        'name': [parties_by_vote['name'].iloc[0]],
+        'other': [0],
+        'cpc': [0],
+        'lpc': [0],
+        'ndp': [0],
+        'gpc': [0],
+        'bq': [0],
+    }
+    votes = 0
+    for (party, series) in parties_by_vote.iterrows():
+        votes += series['votes']
+        if party in parties:
+            district[parties[party]][0] = series['votes']
+        else:
+            district['other'][0] += series['votes']
+    district['votes'] = [votes]
+    district['prov'] = [int(str(district['id'][0])[:2])]
+
+    districts = districts.append(pd.DataFrame.from_records(district, index='id'))
+
+districts.to_csv('data/results_2015.csv', index_label='id', columns=district_headers)
